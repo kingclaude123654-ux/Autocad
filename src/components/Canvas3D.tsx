c35495c711cdaa8916868c550129f7dfb7b4e92c
kingclaude123654-ux <kingclaude123654@gmail.com>
2026-06-16 05:16:12 +0300

    Create Canvas3D.tsx

---
+++ src/components/Canvas3D.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Canvas3DProps {
+  containerRef: React.RefObject<HTMLDivElement | null>;
+  onCanvasClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+export const Canvas3D: React.FC<Canvas3DProps> = ({ containerRef, onCanvasClick }) => {
+  return (
+    <div 
+      ref={containerRef} 
+      onClick={onCanvasClick}
+      className="flex-1 w-full h-full relative cursor-crosshair overflow-hidden touch-none select-none"
+    />
+  );
+};
